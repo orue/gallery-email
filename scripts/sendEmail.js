afb400a47119bbecadf7c224c509bfe7813fbbb5
Carlos Orue <carlos@orue.io>
2022-04-15 09:27:48 -0500

    trying difrents layouts

--- scripts/sendEmail.js
+++ scripts/sendEmail.js
@@ -3,36 +3,36 @@ const env = require("dotenv").config();
 const nodemailer = require("nodemailer");
 
 async function sendEmail(htmlPath) {
-    try {
-        const mailConfig = {
-            user: env.parsed.SMTP_USER,
-            password: env.parsed.SMTP_PASSWORD,
-            supportEmailFrom: env.parsed.EMAIL_FROM_ADDRESS,
-            supportEmailTo: env.parsed.EMAIL_TO_ADDRESS,
-        }
-        
-        const htmlBody = fs.readFileSync(htmlPath, "utf8");
+  try {
+    const mailConfig = {
+      user: env.parsed.SMTP_USER,
+      password: env.parsed.SMTP_PASSWORD,
+      supportEmailFrom: env.parsed.EMAIL_FROM_ADDRESS,
+      supportEmailTo: env.parsed.EMAIL_TO_ADDRESS,
+    };
 
-        const mailTransport = nodemailer.createTransport({
-            service: "gmail",
-            port: 465,
-            auth: {
-                user: mailConfig.user,
-                pass: mailConfig.password,
-            },
-        });
-    
-        const mailOptions = {
-            to: mailConfig.supportEmailTo,
-            from: mailConfig.supportEmailFrom,
-            subject: "this is a test email",
-            html: htmlBody,
-        };
+    const htmlBody = fs.readFileSync(htmlPath, "utf8");
 
-        await mailTransport.sendMail(mailOptions);
-    } catch (error) {
-        console.error(error);
-    }
+    const mailTransport = nodemailer.createTransport({
+      service: "gmail",
+      port: 465,
+      auth: {
+        user: mailConfig.user,
+        pass: mailConfig.password,
+      },
+    });
+
+    const mailOptions = {
+      to: mailConfig.supportEmailTo,
+      from: mailConfig.supportEmailFrom,
+      subject: "this is a test email",
+      html: htmlBody,
+    };
+
+    await mailTransport.sendMail(mailOptions);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-sendEmail("../index.html")
+sendEmail("../index.html");
