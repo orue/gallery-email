777cdacedf94d7471cc32da801baad308a736c66
mrreband <mr@reband.org>
2022-07-28 13:28:29 -0500

    mergeFiles function should return the merged HTML as a string

--- scripts/mergeFiles.js
+++ scripts/mergeFiles.js
@@ -6,12 +6,11 @@ function getHTMLContent(filePath) {
     const content = parse(fs.readFileSync(filePath, "utf8"))
     const head = content.getElementsByTagName("head")[0]
     const styles = head.getElementsByTagName("style")
-    
     const body = content.getElementsByTagName("body")[0]
     return {content, head, styles, body}
 }
 
-async function mergeFiles(staticContentPath, dynamicContentPath){
+function mergeFiles(staticContentPath, dynamicContentPath){
     const {content: staticContent, head: staticHead, styles: staticHeadStyles, body: staticBody} = getHTMLContent(staticContentPath)
     const {content: dynamicContent, head: dynamicHead, styles: dynamicHeadStyles, body: dynamicBody} = getHTMLContent(dynamicContentPath)
     
@@ -24,9 +23,8 @@ async function mergeFiles(staticContentPath, dynamicContentPath){
     // insert dynamic body between the static body's header and footer
     staticBody.getElementById("body-dynamic").replaceWith(dynamicBody);
 
-    // console.log({staticContent, dynamicContent, staticHead, dynamicHead, staticBody, dynamicBody: dynamicBody})
-
-    fs.writeFileSync("./output/output.html", staticContent.outerHTML, "utf-8")
+    return staticContent.outerHTML
 }
 
-mergeFiles("../index.html", "../table.html")
+const outputHTML = mergeFiles("../index.html", "../table.html")
+fs.writeFileSync("./output/output.html", outputHTML, "utf-8")
